6db0e50f2c14cbe203cc66d6c3308a984c069192
WillHayCode <WillHayCode@gmail.com>
2020-03-31 17:49:48 +1000

    +Transferred project to TypeScript

--- background.js
+++ background.js
@@ -1,31 +1,19 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
-//example of using a message handler from the inject scripts
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      //chrome.pageAction.show(sender.tab.id);
-      console.log('background.js');
-      console.log('request')
-      console.log(request)
-      console.log('sender')
-      console.log(sender)
-      
-      console.log('sendResponse')
-      console.log(sendResponse)
-      sendResponse();
+"use strict";
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    console.log('background.js');
+    console.log('request');
+    console.log(request);
+    console.log('sender');
+    console.log(sender);
+    console.log('sendResponse');
+    console.log(sendResponse);
+    sendResponse();
 });
-
-chrome.runtime.onInstalled.addListener(function (object) {
-    chrome.runtime.openOptionsPage(/*callback*/)
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.runtime.openOptionsPage( /*callback*/);
 });
-
-  // Called when the user clicks on the browser action icon.
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.runtime.openOptionsPage(/*callback*/)
+// Called when the user clicks on the browser action icon.
+chrome.browserAction.onClicked.addListener(function () {
+    chrome.runtime.openOptionsPage( /*callback*/);
     //openOrFocusOptionsPage();
 });
