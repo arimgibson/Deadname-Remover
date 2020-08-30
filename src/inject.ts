2523abf4147e0644eb8d9e342d5b25e1df342599
Gusted <williamzijl7@hotmail.com>
2020-08-30 13:07:11 +0200

    no message

--- src/inject.ts
+++ src/inject.ts
@@ -5,6 +5,8 @@ let alivename = null;
 let deadname = null;
 
 function start() {
+    // Some wacky hack to get a click-able link to the inject.js source code in Firefox that don't load side-loaded add-ons in Files.
+    console.log('Starting.');
     let disabled = false;
     chrome.storage.sync.get(DEFAULT_SETTINGS, (sync: UserSettings) => {
         if (sync.enabled == false) {
@@ -57,11 +59,18 @@ function changeContent() {
 
 function checkNodeForReplacement(node: Node, dead: RegExp[], replacement: string[]) {
     if (node.nodeType === 3) {
+        console.log(node);
         for (let i = 0, len = dead.length; i < len; i++) {
             const text = node.nodeValue;
             const newText = text.replace(dead[i], replacement[i]);
             if (newText !== text) {
                 node.parentElement.replaceChild(document.createTextNode(newText), node);
+            }
+        }
+    } else {
+        if (node.hasChildNodes()) {
+            for (let i = 0, len = node.childNodes.length; i < len; i++) {
+                checkNodeForReplacement(node.childNodes[i], dead, replacement);
             }
         }
     }
@@ -71,49 +80,45 @@ function setupListener(dead: RegExp[], replacement: string[]) {
     const observer = new MutationObserver((mutations: Array<MutationRecord>) => {
         for (let i = 0, len = mutations.length; i < len; i++) {
             const mutation: MutationRecord = mutations[i];
-            mutation.addedNodes.forEach((node: Node) =>{
-                checkNodeForReplacement(node, dead, replacement);
-            });
+            if (mutation.type === 'childList') {
+                mutation.addedNodes.forEach((node: Node) => {
+                    checkNodeForReplacement(node, dead, replacement);
+                });
+            }
         }
     });
     observer.observe(document, {childList: true, subtree: true});
 }
 
+function checkElementForTextNodes(dead: RegExp[], replacement: string[]) {
+	var elements = document.body.getElementsByTagName("*");
+	for (let i = 0, len = elements.length; i < len; i++) {
+		let element = elements[i];
+		let children = element.childNodes;
+		for (let n = 0, len2 = children.length; n < len2; n++) {
+			checkNodeForReplacement(children[n], dead, replacement);
+		}
+	};
+    setupListener(dead, replacement);
+}
 
 function replaceNames(old: string[], replacement: string[]) {
     const dead = [];
     for (let i = 0, len = old.length; i < len; i++) {
         dead.push(new RegExp("\\b" + old[i] + "\\b", "gi"));
     }
-
     if (!isDOMReady()) {
         addDOMReadyListener(() => {
             for (let i = 0, len = dead.length; i < len; i++) {
                 document.title = document.title.replace(dead[i], replacement[i]);
             }
-            const elements = document.body.getElementsByTagName("*");
-            for (let i = 0, len = elements.length; i < len; i++) {
-                let element = elements[i];
-                let children = element.childNodes;
-                for (let n = 0, len2 = children.length; n < len2; n++) {
-                    checkNodeForReplacement(children[n], dead, replacement);
-                }
-            };
-            setupListener(dead, replacement);
+            checkElementForTextNodes(dead, replacement);
         })
     } else {
         for (let i = 0, len = dead.length; i < len; i++) {
             document.title = document.title.replace(dead[i], replacement[i]);
         }
-        const elements = document.body.getElementsByTagName("*");
-        for (let i = 0, len = elements.length; i < len; i++) {
-            let element = elements[i];
-            let children = element.childNodes;
-            for (let n = 0, len2 = children.length; n < len2; n++) {
-                checkNodeForReplacement(children[n], dead, replacement);
-            }
-        };
-        setupListener(dead, replacement);
+        checkElementForTextNodes(dead, replacement);
     }
 }
 
