b4f18f4a5c151679b169e596acf66dfe4b95cd8c
Gusted <williamzijl7@hotmail.com>
2020-09-14 22:25:29 +0200

    - Make the setting variable converter automatic - Ignore api.js - Fix path to generateUserJS.js

--- generateUserJS.js
+++ generateUserJS.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
 const pjson = require('./package.json');
+const types = require('./build/Types');
 
 const fileData = fs.readFileSync('build\\api.js', {encoding: 'utf8'});
+let settings = JSON.stringify(types.DEFAULT_SETTINGS, null, '\t').split('\n')
+
+for (let x = 0, len = settings.length; x < len; x++) {
+    settings[x] = settings[x].replace(/.+?(?=\:)/g, (m$) => {
+        console.log(m$);
+        return m$.replace(/\"/g, '');
+    });
+}
+
 const data = [
     '// ==UserScript==',
     '// @name         Deadname-Remover',
@@ -14,19 +24,7 @@ const data = [
     '',
     '(function() {',
     "\t'use strict';",
-    `\tvar settings = {`,
-    '\t\tname: {',
-    '\t\t\tfirst: "",',
-    '\t\t\tmiddle: "",',
-    '\t\t\tlast: "",',
-    '\t\t},',
-    '\t\tdeadname: {',
-    '\t\t\tfirst: "",',
-    '\t\t\tmiddle: "",',
-    '\t\t\tlast: "",',
-    '\t\t},',
-    '\t\tenabled: true',
-    '\t}',
+    `\tvar settings = ${settings.join('\n')}`,
     `\t${fileData}`,
     '\tDeadnameRemover.start(settings);',
     '})();',
