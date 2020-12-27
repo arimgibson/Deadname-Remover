c863028c0597ae7b84ac8a771afa92c45dc241b2
Gusted <williamzijl7@hotmail.com>
2020-12-27 22:44:05 +0100

    Handle path's with spaces
    - Fixes an issue where the path to `node.exe` had an space in it and will be recognized as an argument.
    - Resolves #259

--- tasks/type-checking.js
+++ tasks/type-checking.js
@@ -2,7 +2,7 @@ const {exec} = require('child_process');
 
 async function typeCheck() {
     await new Promise((resolve, reject) => {
-        exec(`${process.execPath} ${require.resolve('typescript/lib/tsc.js')} --project "tsconfig.json"`, (err) => {
+        exec(`"${process.execPath}" "${require.resolve('typescript/lib/tsc.js')}" --project "tsconfig.json"`, (err) => {
             if (err) {
                 reject(new Error(`tsc exited with error: ${err}`));
             } else {
