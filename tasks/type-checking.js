29fac95c31bedae780a4602cc37cb0493c73818d
Gusted <williamzijl7@hotmail.com>
2021-01-06 00:25:15 +0100

    Remove potential security flaw
    The old code could be attacked with command injection. Howell it's uncommon to happen it's better safe than sorry.
    Read more about: 
    https://owasp.org/www-community/attacks/Command_Injection
    https://cwe.mitre.org/data/definitions/78.html
    https://cwe.mitre.org/data/definitions/88.html

--- tasks/type-checking.js
+++ tasks/type-checking.js
@@ -1,10 +1,10 @@
-const {exec} = require('child_process');
+const {execFile} = require('child_process');
 
 async function typeCheck() {
     await new Promise((resolve, reject) => {
-        exec(`"${process.execPath}" "${require.resolve('typescript/lib/tsc.js')}" --project "tsconfig.json"`, (err) => {
+        execFile(process.execPath, [require.resolve('typescript/lib/tsc.js'), '--project', 'tsconfig.json'], (err) => {
             if (err) {
-                reject(new Error(`tsc exited with error: ${err}`));
+                reject(new Error(`tsc has exited with error: ${err.message}`));
             } else {
                 resolve();
             }
