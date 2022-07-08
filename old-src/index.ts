eed8cafde98e078dbd806efd1230f301002867aa
Ari Gibson <hi@arimgibson.com>
2022-07-08 05:43:21 -0600

    Styling changes, src from scratch

---
+++ old-src/index.ts
@@ -0,0 +1,13 @@
+import {start} from './inject/inject';
+
+if (!window.chrome) {
+    window.chrome = {} as any;
+}
+if (!chrome.runtime) {
+    chrome.runtime = {} as any;
+}
+
+const port = chrome.runtime.connect({name: 'tab'});
+port.onMessage.addListener((settings) => {
+    start(settings);
+});
---
+++ old-src/popup/stealth.ts
@@ -0,0 +1,5 @@
+const port = chrome.runtime.connect({name: 'popup'});
+
+document.querySelector('input[type="button"]').addEventListener('click', () => {
+    port.postMessage({type: 'save-data', data: {stealthMode: (document.querySelector('.onoff-option') as HTMLInputElement).checked}});
+});
