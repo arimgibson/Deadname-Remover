8866c4ebe34e330646a268df76f013a4b2a19f28
Ari Gibson <hi@arimgibson.com>
2022-07-08 06:06:03 -0600

    Starting off with styling updates

---
+++ src/types.ts
@@ -0,0 +1,33 @@
+export interface Name {
+  first: string;
+  middle: string;
+  last: string;
+}
+
+export interface UserSettings {
+  name: Name;
+  deadname: Name[];
+  enabled: boolean;
+  stealthMode: boolean;
+  highlight: boolean;
+  theme: "trans" | "non-binary" | "high-contrast-light" | "hight-contrast-dark";
+}
+
+export const DEFAULT_SETTINGS: UserSettings = {
+  name: {
+    first: "",
+    middle: "",
+    last: "",
+  },
+  deadname: [
+    {
+      first: "",
+      middle: "",
+      last: "",
+    },
+  ],
+  enabled: true,
+  stealthMode: false,
+  highlight: false,
+  theme: "trans",
+};
