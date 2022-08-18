65cb65b4602ed5e593e850a9779de37d623f225f
Ari Gibson <hi@arimgibson.com>
2022-08-17 18:38:46 -0600

    Potentially fixed bug with shadowdom

--- src/parser.ts
+++ src/parser.ts
@@ -9,7 +9,7 @@ export default function walk(node: Document | Node): void {
     case 1: // Element
     case 9: // Document
     case 11: // Document fragment
-      child = node.firstChild;
+      child = node.firstChild ?? (node as Element).shadowRoot;
       while (child) {
         next = child.nextSibling;
         walk(child);
