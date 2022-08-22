71df69d0842266746c7183fabf430a8ba1dd76e2
Ari Gibson <hi@arimgibson.com>
2022-08-22 10:50:36 -0600

    Updated references from `master` to `main`

--- PRIVACY_POLICY.md
+++ PRIVACY_POLICY.md
@@ -6,4 +6,4 @@ Deadname-Remover had never collected and will never collect any personal data, b
 
 Deadname-Remover uses Chrome (Chromium) or WebExtensions Storage Sync API for storing user's settings.
 
-###### Updated: January 30, 2021 [(see update history)](https://github.com/WillHayCode/Deadname-Remover/commits/master/PRIVACY_POLICY.md)
+###### Updated: January 30, 2021 [(see update history)](https://github.com/WillHayCode/Deadname-Remover/commits/main/PRIVACY_POLICY.md)
--- deadname-remover.user.js
+++ deadname-remover.user.js
@@ -9,9 +9,9 @@
 // @run-at       document-start
 // @namespace    https://github.com/arimgibson/Deadname-Remover
 // @supportURL   https://github.com/arimgibson/Deadname-Remover/issues
-// @require      https://github.com/arimgibson/Deadname-Remover/raw/master/deadname-remover.require.js
-// @updateURL    https://github.com/arimgibson/Deadname-Remover/raw/master/deadname-remover.meta.js
-// @downloadURL  https://github.com/arimgibson/Deadname-Remover/raw/master/deadname-remover.user.js
+// @require      https://github.com/arimgibson/Deadname-Remover/raw/main/deadname-remover.require.js
+// @updateURL    https://github.com/arimgibson/Deadname-Remover/raw/main/deadname-remover.meta.js
+// @downloadURL  https://github.com/arimgibson/Deadname-Remover/raw/main/deadname-remover.user.js
 // ==/UserScript==
 
 (function() {
