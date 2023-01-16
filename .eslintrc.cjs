546ab26363e24df5cc37554ef82c94c2b6156963
Ari Gibson <hi@arimgibson.com>
2023-01-15 22:18:45 -0700

    Adding lint configs & scripts

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'airbnb-typescript/base'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json', './tsconfig.eslint.json'],
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+  // ignorePatterns: ['.eslintrc.cjs', 'old-src'],
+};
