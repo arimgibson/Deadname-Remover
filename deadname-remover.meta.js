3bfceb2b5127c5d315c7ac66b2e2989f3e4897c1
Ari Gibson <hi@arimgibson.com>
2025-02-16 01:49:24 -0700

    Clear main branch for v2 -- incompat histories :(

--- deadname-remover.meta.js
+++
@@ -1,5 +0,0 @@
-// ==UserScript==
-// @name        Deadname-Remover
-// @version     1.1.2
-// @namespace   https://github.com/WillHayCode/Deadname-Remover
-// ==/UserScript==
--- src/popup/stealth.ts
+++
@@ -1,5 +0,0 @@
-const port = chrome.runtime.connect({ name: 'popup' });
-
-document.querySelector('input[type="button"]').addEventListener('click', () => {
-  port.postMessage({ type: 'save-data', data: { stealthMode: (document.querySelector('.onoff-option') as HTMLInputElement).checked } });
-});
