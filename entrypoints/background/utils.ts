e4530359efbb1e583dd8f13e70092728041cfdbb
Lenoch <contact.lenoch@gmail.com>
2025-03-05 01:14:22 +0100

    Fix: Ensure config.hideDebugInfo is not undefined

--- entrypoints/background/utils.ts
+++ entrypoints/background/utils.ts
@@ -22,6 +22,12 @@ export async function handleUpdate(details: Runtime.OnInstalledDetailsType) {
 
   let config = await getConfig()
 
+  // Ensure the specific value is set to false if undefined
+  if (config.hideDebugInfo === undefined) {
+    config.hideDebugInfo = false
+    await setConfig(config)
+  }
+
   // If last version is less than 2.0.0, open the options page with the upgrade flag
   if (details.previousVersion && compare(details.previousVersion, '2.0.0', '<')) {
     // If stealth mode is disabled, open the options page with the upgrade flag
