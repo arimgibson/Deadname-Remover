fdbec606f817cdc121df586ecfc75de9f4fda8c6
Ari Gibson <hi@arimgibson.com>
2025-05-04 20:50:57 -0600

    bump deps, resolve compat issues

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vitest/config'
 import { WxtVitest } from 'wxt/testing'
 
 export default defineConfig({
+  // @ts-expect-error -- version mismatches, not a big deal. Should be resolved soon
   plugins: [WxtVitest()],
 })
