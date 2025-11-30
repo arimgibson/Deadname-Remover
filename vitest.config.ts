cc182762dab00316bffd0b028643d5da2d8fb600
Ari Gibson <hi@arimgibson.com>
2025-11-29 23:00:23 -0700

    Bump Deps on `feature/2.2.0` (#639)
    * fix: bump all deps to latest
    
    * fix: updates in response to dep updates

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from 'vitest/config'
 import { WxtVitest } from 'wxt/testing'
 
 export default defineConfig({
-  // @ts-expect-error -- version mismatches, not a big deal. Should be resolved soon
   plugins: [WxtVitest()],
 })
