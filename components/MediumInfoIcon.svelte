17ea737555e185a8edd4868b52d8d25e95ff027a
Ari Gibson <hi@arimgibson.com>
2025-11-30 19:34:37 -0700

    fix: (coderabbit) icon accessibility

--- components/MediumInfoIcon.svelte
+++ components/MediumInfoIcon.svelte
@@ -1,1 +1,1 @@
-<i class="i-material-symbols-info-outline w-10"></i>
+<i class="i-material-symbols-info-outline w-10" aria-hidden="true" role="img"></i>
--- components/SmallInfoIcon.svelte
+++ components/SmallInfoIcon.svelte
@@ -1,1 +1,1 @@
-<i class="i-material-symbols-info-outline h-6 w-6"></i>
+<i class="i-material-symbols-info-outline h-6 w-6" aria-hidden="true" role="img"></i>
