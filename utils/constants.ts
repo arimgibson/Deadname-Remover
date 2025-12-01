8055d22be9c86c9635e87a6d50c232127d069d17
Ari Gibson <hi@arimgibson.com>
2025-11-30 18:08:19 -0700

    fix: designate some settings as advanced

--- utils/constants.ts
+++ utils/constants.ts
@@ -21,34 +21,40 @@ export const generalSettingKeys = [
   {
     label: 'Enable Extension',
     value: 'enabled',
+    advanced: false,
     description: 'Enable or disable the extension\'s name replacement functionality.',
   },
   {
     label: 'Stealth Mode',
     value: 'stealthMode',
+    advanced: false,
     description:
       'Hide gender-related elements to protect privacy and accidentally being outed. Replaces the extension icon, and hides the popup options when the extension\'s icon is clicked. Recommended to disable text highlighting as well.',
   },
   {
     label: 'Hide Debug Info',
     value: 'hideDebugInfo',
+    advanced: true,
     description:
       'Hide debug information from the browser console. This can help protect your privacy by not exposing potentially sensitive information.',
   },
   {
     label: 'Block Page Until Replacements Finished',
     value: 'blockContentBeforeDone',
+    advanced: false,
     description:
       'Block the page until all replacements are finished to avoid displaying a deadname before replacement. Can cause slowdowns on lower-end devices.',
   },
   {
     label: 'Highlight Replaced Names',
     value: 'highlightReplacedNames',
+    advanced: false,
     description: 'Highlight replaced names to make them easier to spot.',
   },
   {
     label: 'Sync Settings Across Devices',
     value: 'syncSettingsAcrossDevices',
+    advanced: false,
     description: 'Sync settings across devices signed into the same browser profile (e.g. Google account).',
   },
 ] as const
